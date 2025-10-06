ff098487ae68e81bb02908f80ca4ed97afcb334a
alessa-weiler <148051925+alessa-weiler@users.noreply.github.com>
2025-10-06 15:57:02 +0100

    trying some changes to get her working

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/home/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html'
+      }
+    }
+  }
+});
